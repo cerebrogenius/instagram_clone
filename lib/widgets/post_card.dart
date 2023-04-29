ad2c8fbef874290242ef8baf86443e71fd7aef1c
cerebrogenius <yahkubabdulmajeed1@gmail.com>
2023-04-29 08:14:45 +0100

    ore

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -56,7 +56,76 @@ class PostCard extends StatelessWidget {
                   icon: const Icon(Icons.more_vert),
                 )
               ]),
-            )
+            ),SizedBox(
+              height: MediaQuery.of(context).size.height*0.35,
+              width: double.infinity,
+              child: Image.network(
+                'https://cdn.pixabay.com/photo/2014/12/24/05/02/drop-of-water-578897__480.jpg',
+                fit: BoxFit.cover
+              )
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.favorite,color: Colors.red)
+                    ),
+                     IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.comment_outlined)
+                    ),
+                      IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.send)
+                    ),
+                    Expanded(
+                      child: Align(alignment: Alignment.bottomRight,
+                      child:   IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.bookmark_border)
+                    ),
+                      ),), 
+
+                ]
+              ),
+              Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      DefaultTextStyle(
+                        style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w300),
+                        child: Text(
+                          '1,231 likes',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        padding: const EdgeInsets.only(
+                          top:8
+                        ),
+                        child: RichText(text: const TextSpan(
+                          style: TextStyle(
+                            color: primaryColor
+                          ),
+                          children:  [
+                            TextSpan(
+                              text: 'username',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold
+                              )
+                            ),
+                            TextSpan(text: ':  hey this is some description to be replaced')
+                          ]
+                        )),
+                      )
+                    ],
+                  )
+                )
           ],
         ));
   }
