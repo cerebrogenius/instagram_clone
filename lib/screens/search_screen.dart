b171ba221915381619a1963f68b5eca81ceeadc1
cerebrogenius <yahkubabdulmajeed1@gmail.com>
2023-04-30 16:50:15 +0100

    user search and delete post

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+import '../utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,8 +13,76 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
+
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          onFieldSubmitted: (String _) {
+            setState(() {
+              isShowUsers = true;
+            });
+          },
+          decoration: const InputDecoration(labelText: 'Search for a user'),
+        ),
+      ),
+      body: isShowUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where('username',
+                      isGreaterThanOrEqualTo: searchController.text.toString())
+                  .get(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                    itemCount: snapshot.data?.docs.length ?? 1,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              snapshot.data!.docs[index]['photoUrl']),
+                        ),
+                        title: Text(snapshot.data!.docs[index]['username']),
+                      );
+                    });
+              })
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  staggeredTileBuilder: (index) {
+                    return StaggeredTile.count(
+                        (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1);
+                  },
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    return Image.network(
+                        (snapshot.data! as dynamic).docs[index]['postUrl']);
+                  },
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              },
+            ),
+    );
   }
 }
